830db0f12d17340d0509c8c5f4485b12b03e8460
PALUBO <54085980+P4B0N@users.noreply.github.com>
2019-09-04 21:25:32 -0300

    Add files via upload

--- Semana_4/action_page.php
+++ Semana_4/action_page.php
@@ -3,6 +3,15 @@
 //var_dump($_GET)
 echo"Su nomre es ".$_GET["Nombre"];
 echo"<br>Su apellido es ".$_GET["Apellido"];
+
+
+$miobjeto=new stdClass();
+$miobjeto->Nombre=$_GET['Nombre'];
+//$miobjeto->Apellido=$_GET['Apellido'];
+var_dump(json_encode($miobjeto));
 $archivo=fopen('usuarios.txt','w');
-fwrite($archivo,$_GET['Nombre']."\n");
+fwrite($archivo,json_encode($miobjeto)."\n");
+//fwrite($archivo,$_GET['Apellido']."\n");
+fclose($archivo);
+var_dump($miobjeto);
 ?>
